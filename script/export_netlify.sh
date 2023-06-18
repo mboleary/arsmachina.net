1e5a5e1c34b3226f6640e537bfb7e104086c0526
Brady O'Leary <thebradyoleary@gmail.com>
2023-06-17 22:43:45 -0400

    corrects deploy script

--- script/export_netlify.sh
+++ script/export_netlify.sh
@@ -12,5 +12,5 @@ echo $ACCESS_TOKEN
 
 curl -H "Content-Type: application/zip" \
      -H "Authorization: Bearer $ACCESS_TOKEN" \
-     --data-binary "$DEPLOY_FILE" \
+     --data-binary "@$DEPLOY_FILE" \
      https://api.netlify.com/api/v1/sites/$SITE_ID/deploys
